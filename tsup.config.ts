e658964bb07c1ec916ca7f207e9f8e4569d8bb3d
Douwe Pausma <douwepausma@gmail.com>
2025-11-14 18:59:22 +0100

    restructure files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["index.ts", "client.ts"],
+  entry: ["src/index.ts", "src/client.ts"],
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
